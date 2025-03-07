08e161bdbe7b99e58c5fe62ebb98a8dcea0921cf
aditya <adityakulkarni54321@gmail.com>
2025-03-07 19:09:48 +0530

    changes

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          primary: ['Ubuntu', 'sans-serif'],
+          secondary: ['Mullish', 'sans-serif'],
+        },
+      },
+    },
+  };
